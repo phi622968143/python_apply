42a09c4c2915b5721e07f4b8acebf32e89b60b3d
F54101056 <40814498+phi622968143@users.noreply.github.com>
2023-03-01 00:02:49 +0800

    Create paraF.py

---
+++ paraF.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(-5, 5, 1000)
+xp=(np.sin(t))*(np.exp(np.cos(t)) - 2*np.cos(4*t) - (np.sin(t/12))**5)
+yp=(np.cos(t))*(np.exp(np.cos(t)) - 2*np.cos(4*t) - (np.sin(t/12))**5)
+
+plt.scatter(xp, yp, label='f(x)')
+plt.legend()
+plt.show()
