e06e8e3cdc5933c8fa0ac9514b746d34a4fee050
F54101056 <40814498+phi622968143@users.noreply.github.com>
2023-03-01 00:06:17 +0800

    add photo
    https://ppt.cc/fn7kwx

--- paraF.py
+++ paraF.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+#butterfly
 t = np.linspace(-5, 5, 1000)
 xp=(np.sin(t))*(np.exp(np.cos(t)) - 2*np.cos(4*t) - (np.sin(t/12))**5)
 yp=(np.cos(t))*(np.exp(np.cos(t)) - 2*np.cos(4*t) - (np.sin(t/12))**5)
